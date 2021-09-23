d1e943dc576d654cafc80f97518e31be7caf8609
nhatlongit2k <nhatlongit2k@gmail.com>
2021-09-23 14:13:05 +0700

    nop bai

---
+++ app/src/main/java/com/example/paypalui/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.paypalui
+
+import android.content.res.Configuration
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+//        val navController = findNavController(R.id.fragment)
+//        val appBarConfiguration = AppBarConfiguration(setOf(R.id.fragmentDashboard, R.id.perfil4))
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+//
+//        bottomNavigationView.setupWithNavController(navController)
+
+//        if(resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT)
+    }
+}
