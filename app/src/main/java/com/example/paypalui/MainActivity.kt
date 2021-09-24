7af1bc756246fbb34587db694da258cccba7494c
nhatlongit2k <nhatlongit2k@gmail.com>
2021-09-24 08:46:26 +0700

    no bai

--- app/src/main/java/com/example/paypalui/MainActivity.kt
+++ app/src/main/java/com/example/paypalui/MainActivity.kt
@@ -13,15 +13,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cards)
+        setContentView(R.layout.activity_main)
 
-//        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
-//        val navController = findNavController(R.id.fragment)
-//        val appBarConfiguration = AppBarConfiguration(setOf(R.id.fragmentDashboard, R.id.perfil4))
-//        setupActionBarWithNavController(navController, appBarConfiguration)
-//
-//        bottomNavigationView.setupWithNavController(navController)
-
-//        if(resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT)
     }
 }
