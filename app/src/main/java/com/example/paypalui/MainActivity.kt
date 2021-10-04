70687bfa0a5d50be548cb60fa7d1ca86455603c2
LongTienSinh <nhatlongit2k@gmail.com>
2021-10-05 02:18:00 +0700

    Sua loi giao dien

--- app/src/main/java/com/example/paypalui/MainActivity.kt
+++ app/src/main/java/com/example/paypalui/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.paypalui
 
+import android.content.Intent
 import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,11 +10,18 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var floatingActionButton: FloatingActionButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        floatingActionButton = findViewById(R.id.dashboard_fab)
+        floatingActionButton.setOnClickListener {
+            val intent = Intent(this, PayActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/paypalui/PayActivity.kt
+++ app/src/main/java/com/example/paypalui/PayActivity.kt
@@ -1,11 +1,20 @@
 package com.example.paypalui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class PayActivity : AppCompatActivity() {
+
+    lateinit var tvEnviar: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pay)
+        tvEnviar = findViewById(R.id.txEnviar)
+        tvEnviar.setOnClickListener {
+            val intent = Intent(this, CardsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
